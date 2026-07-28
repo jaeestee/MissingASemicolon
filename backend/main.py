3aa3e46c0dc31ff65326ea631937eb27fd13ae07
mtm162 <mtm162@case.edu>
2026-07-27 20:44:27 -0400

    this is the v0 code

--- backend/main.py
+++
@@ -1,7 +0,0 @@
-"""Main entry point to run the FastAPI application."""
-
-import uvicorn
-from backend.game_engine.service import app
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
